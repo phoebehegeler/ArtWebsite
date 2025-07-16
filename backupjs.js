9e22faba5eadf838b03a156b015f22b70582c4be
phoebehegeler <phoebe.hegeler2006@gmail.com>
2025-07-17 00:23:05 +0800

    Add files via upload

---
+++ backupjs.js
@@ -0,0 +1,59 @@
+/*var ppich = document.querySelectorAll('.subpageclasshi');
+var ppch = document.querySelectorAll('.subpagehello');
+
+function subpagepastelpink() {
+	document.body.style.background = "#fff0f2";
+	for (let i = 0; i < ppich.length ; i++) {
+	  document.getElementsByClassName("subpageclasshi")[i].style.color = "#57042e";
+	  }
+	for (let i = 0; i < ppch.length; i++) {
+	/*button text*/
+    document.getElementsByClassName("subpagehello")[i].style.color = "white";
+    } 
+	
+	  }
+	  */
+
+/*var ppich = document.querySelectorAll('.subpageclasshi');
+var ppch = document.querySelectorAll('.subpagehello');
+
+function subpagepastelpink() {
+    document.body.style.background = "#fff0f2";
+
+    for (let i = 0; i < ppich.length; i++) {
+        ppich[i].style.color = "#57042e";
+    }
+
+    for (let i = 0; i < ppch.length; i++) {
+        /* button text */
+        ppch[i].style.color = "white";
+    }
+}*/
+
+/*function subpagepastelpink() {
+    document.body.style.background = "#fff0f2";
+
+    var subpageclasshiElements = document.getElementsByClassName("subpageclasshi");
+    for (let i = 0; i < subpageclasshiElements.length; i++) {
+        subpageclasshiElements[i].style.color = "#57042e";
+    }
+
+    var subpagehelloElements = document.getElementsByClassName("subpagehello");
+    for (let i = 0; i < subpagehelloElements.length; i++) {
+        subpagehelloElements[i].style.color = "white";
+    }
+}
+*/
+	
+	
+	function subpagepastelpink() {
+	document.body.style.background = "#fff0f2";
+	for (let i = 0; i < 18 ; i++) {
+	  document.getElementsByClassName("subpageclasshi")[i].style.color = "#57042e";
+	  }
+	for (let i = 0; i < 2; i++) {
+	/*button text*/
+    document.getElementsByClassName("subpagehello")[i].style.color = "white";
+    } 
+	
+	  }
---
+++ colorschemejs.js
@@ -0,0 +1,198 @@
+function white() {
+	document.body.style.background = "#FFFFFF";
+	 for (let i = 0; i < 15; i++) {
+    document.getElementsByClassName("hello")[i].style.background = "#5A5A5A";
+    } 
+	for (let i = 0; i < 6; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "black";
+	  }
+	for (let i = 6; i < 7; i++) {
+	document.getElementsByClassName("classhi")[i].style.color = 'white';
+	  }
+	for (let i = 7; i < 11; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "black";
+	  }
+	for (let i = 11; i < 13; i++) {
+	document.getElementsByClassName("classhi")[i].style.color = 'white';
+	  }
+	for (let i = 13; i < 19; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "black";
+	  }
+	for (let i = 19; i < 31; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "white";
+	  }
+	for (let i = 31; i < 39; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "black";
+	  }
+	}
+	}
+
+
+function black() {
+	document.body.style.background = "#000000"
+	for (let i = 0; i < 15; i++) {
+    document.getElementsByClassName("hello")[i].style.background = "white";
+    } 
+	for (let i = 0; i < 5; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = 'white';
+	  }
+	for (let i = 4; i < 34; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = 'black';
+	  }
+	for (let i = 34; i < 39; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = 'white';
+	  }
+	}
+}
+
+function brown() {
+	document.body.style.background = "#FFFFFF"
+	 for (let i = 0; i < 15; i++) {
+    document.getElementsByClassName("hello")[i].style.background = "#3d051b";
+    } 
+	for (let i = 0; i < 6; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "#3d051b";
+	  }
+	for (let i = 6; i < 7; i++) {
+	document.getElementsByClassName("classhi")[i].style.color = 'white';
+	  }
+	for (let i = 7; i < 11; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "#3d051b";
+	  }
+	for (let i = 11; i < 13; i++) {
+	document.getElementsByClassName("classhi")[i].style.color = 'white';
+	  }
+	for (let i = 13; i < 19; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "#3d051b";
+	  }
+	for (let i = 19; i < 31; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "white";
+	  }
+	for (let i = 31; i < 39; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "#3d051b";
+	  }
+	}
+
+}
+
+
+function pastelpink() {
+	document.body.style.background = "#fff0f2"
+	 for (let i = 0; i < 15; i++) {
+    document.getElementsByClassName("hello")[i].style.background = "#57042e";
+    } 
+	for (let i = 0; i < 6; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "#57042e";
+	  }
+	for (let i = 6; i < 7; i++) {
+	document.getElementsByClassName("classhi")[i].style.color = "white";
+	  }
+	for (let i = 7; i < 11; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "#57042e";
+	  }
+	for (let i = 11; i < 13; i++) {
+	document.getElementsByClassName("classhi")[i].style.color = "white";
+	  }
+	for (let i = 13; i < 19; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "#57042e";
+	  }
+	for (let i = 19; i < 31; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "white";
+	  }
+	for (let i = 31; i < 39; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "#57042e";
+	  }
+	}
+}
+
+function pastelpurple() {
+	document.body.style.background = "#f6e1fc"
+	 for (let i = 0; i < 15; i++) {
+    document.getElementsByClassName("hello")[i].style.background = "#2a0340";
+    } 
+	for (let i = 0; i < 6; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "#2a0340";
+	  }
+	for (let i = 6; i < 7; i++) {
+	document.getElementsByClassName("classhi")[i].style.color = "white";
+	  }
+	for (let i = 7; i < 11; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "#2a0340";
+	  }
+	for (let i = 11; i < 13; i++) {
+	document.getElementsByClassName("classhi")[i].style.color = "white";
+	  }
+	for (let i = 13; i < 19; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "#2a0340";
+	  }
+	for (let i = 19; i < 31; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "white";
+	  }
+	for (let i = 31; i < 39; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "#2a0340";
+	  }
+	}
+
+}
+
+
+function pastelblue() {
+	document.body.style.background = "#f2f0ff"
+	  for (let i = 0; i < 15; i++) {
+    document.getElementsByClassName("hello")[i].style.background = "#031245";
+    } 
+	for (let i = 0; i < 6; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "#031245";
+	  }
+	for (let i = 6; i < 7; i++) {
+	document.getElementsByClassName("classhi")[i].style.color = "white";
+	  }
+	for (let i = 7; i < 11; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "#031245";
+	  }
+	for (let i = 11; i < 13; i++) {
+	document.getElementsByClassName("classhi")[i].style.color = "white";
+	  }
+	for (let i = 13; i < 19; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "#031245";
+	  }
+	for (let i = 19; i < 31; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "white";
+	  }
+	for (let i = 31; i < 39; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "#031245";
+	  }
+	}
+}
+
+
+function pastelgreen() {
+	document.body.style.background = "#dff7df"
+	  for (let i = 0; i < 15; i++) {
+    document.getElementsByClassName("hello")[i].style.background = "#052b04";
+    } 
+	for (let i = 0; i < 6; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "#052b04";
+	  }
+	for (let i = 6; i < 7; i++) {
+	document.getElementsByClassName("classhi")[i].style.color = "white";
+	  }
+	for (let i = 7; i < 11; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "#052b04";
+	  }
+	for (let i = 11; i < 13; i++) {
+	document.getElementsByClassName("classhi")[i].style.color = "white";
+	  }
+	for (let i = 13; i < 19; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "#052b04";
+	  }
+	for (let i = 19; i < 31; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "white";
+	  }
+	for (let i = 31; i < 39; i++) {
+	  document.getElementsByClassName("classhi")[i].style.color = "#052b04";
+	  }
+	}
+}
+
+	}
