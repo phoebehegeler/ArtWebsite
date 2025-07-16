88255e76464a42600e226807f8b36a85f18a650a
phoebehegeler <phoebe.hegeler2006@gmail.com>
2025-07-17 00:35:59 +0800

    Add files via upload

---
+++ zoominandoutjs.js
@@ -0,0 +1,15 @@
+
+				var zoom = 1;
+		
+		$('.zoom').on('click', function(){
+			zoom += 0.1;
+			$('.target').css('transform', 'scale(' + zoom + ')');
+		});
+		$('.zoom-init').on('click', function(){
+			zoom = 1;
+			$('.target').css('transform', 'scale(' + zoom + ')');
+		});
+		$('.zoom-out').on('click', function(){
+			zoom -= 0.1;
+			$('.target').css('transform', 'scale(' + zoom + ')');
+		});
